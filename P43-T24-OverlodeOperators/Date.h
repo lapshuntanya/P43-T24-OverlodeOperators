a8fb5ab96cd5c5a625ad7589eba84c4bd8ea6480
lapshun <lapshun.tatiana@gmail.com>
2025-09-03 16:52:20 +0300

    Date

---
+++ P43-T24-OverlodeOperators/Date.h
@@ -0,0 +1,32 @@
+#pragma once
+#include <iostream>
+#include <ctime>
+using namespace std;
+
+class Date
+{
+	int year;
+	int month;
+	int day;
+public:
+	//--------- КОНСТРУКТОРИ та ДЕСТРУКТОРИ ---------
+	Date(); // За замовчуванням - поточна дата
+	Date(int year, int month, int day); // З параметрами - задана дата	
+
+	bool isLeapYear()const; //Перевірка на високосний рік
+	int monthDays()const; //Кількість днів за місяцем
+	void nextDate(); // Наступна дата
+	void prevDate(); // Попередня дата
+
+	//--------- АКСЕСОРИ ---------
+	void setYear(int year);
+	int getYear()const;
+	void setMonth(int month);
+	int getMonth()const;
+	void setDay(int day);
+	int getDay()const;
+
+	void showDate()const;
+	bool valid()const; // Перевірка дати на коректність
+};
+
