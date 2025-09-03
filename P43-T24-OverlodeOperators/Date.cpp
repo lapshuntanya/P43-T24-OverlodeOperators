0e9dced5fcf7c9be5ae2296d3308d4ad1996970c
lapshun <lapshun.tatiana@gmail.com>
2025-09-03 16:56:49 +0300

    date cpp

--- P43-T24-OverlodeOperators/Date.cpp
+++ P43-T24-OverlodeOperators/Date.cpp
@@ -1,1 +1,69 @@
 #include "Date.h"
+
+Date::Date()
+{
+    auto pc_time = time(NULL);
+    tm* obj = new tm;
+   
+    localtime_s(obj, &pc_time); //VS
+   // obj = localtime(&pc_time); //Xcode, Clion, Online
+
+}
+
+Date::Date(int year, int month, int day)
+{
+}
+
+bool Date::isLeapYear() const
+{
+    return false;
+}
+
+int Date::monthDays() const
+{
+    return 0;
+}
+
+void Date::nextDate()
+{
+}
+
+void Date::prevDate()
+{
+}
+
+void Date::setYear(int year)
+{
+}
+
+int Date::getYear() const
+{
+    return 0;
+}
+
+void Date::setMonth(int month)
+{
+}
+
+int Date::getMonth() const
+{
+    return 0;
+}
+
+void Date::setDay(int day)
+{
+}
+
+int Date::getDay() const
+{
+    return 0;
+}
+
+void Date::showDate() const
+{
+}
+
+bool Date::valid() const
+{
+    return false;
+}
